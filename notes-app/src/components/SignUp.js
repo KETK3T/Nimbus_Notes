c64e6caf71b34237c781c47864554705875ff62c
jxo5716 <joladiplenty@gmail.com>
2024-10-03 22:11:41 -0500

    wasted time :c
    Created signup page and changed css

--- notes-app/src/components/SignUp.js
+++ notes-app/src/components/SignUp.js
@@ -1,18 +1,71 @@
-import React from "react"
+import React, { useState } from "react"
 import { useNavigate,Link } from "react-router-dom"
 
 const SignUp = () =>{
+
+	const [formData, setFormData] = useState({
+		username:'',
+		email:''
+	})
+
+	const navigate = useNavigate()
+
+	const handleChange = (event) => {
+		const {name, value} = event.target
+		setFormData({
+			...formData,
+			[name]: value
+		})
+	}
+
+	const handleSubmit = (event) =>{
+		// prevent refresh
+		event.preventDefault()
+
+		// store username and email in variables
+		const {username,email} = formData
+		
+		console.log(username)
+		console.log(email)
+		
+		navigate('/home');
+	}
+
+
+
 	return(
 		<div className="signup">
 			<div className="signup-container">
 				<h2>Sign Up</h2>
-				<form>
-					<div>
+				<form onSubmit={handleSubmit}>
+					<div className="form-control">
 						<label className="signup-label">
-							Name:
+							Username:
 						</label>
-						<input className="signup-input"/>
+						<input 
+							className="signup-input"
+							name="username"
+							value={formData.username}
+							onChange={handleChange}
+							type="text"
+							required
+						/>
 					</div>
+
+					<div className="form-control">
+						<label className="signup-label">
+							Email:
+						</label>
+						<input 
+							className="signup-input"
+							name="email"
+							value={formData.email}
+							onChange={handleChange}
+							type="text"
+							required
+						/>
+					</div>
+					<button type='submit' className="save">Submit</button>
 				</form>
 			</div>
 		</div>
